e6a8f8842006e6d68c737939373f966dc57dcb81
amitmnayakacc <90895195+amitmnayakacc@users.noreply.github.com>
2021-10-12 16:07:50 +0530

    Update jenkins_changelog_test.sql

--- jenkins_changelog_test.sql
+++ jenkins_changelog_test.sql
@@ -1,1 +1,19 @@
+-- liquibase formatted sql changeLogId:c0e50faa-d6ec-478e-bdc7-af6474323351
 
+--changeset amit:1
+create table Jenkins_table1 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
+
+--changeset nayak:2
+create table Jenkins_Table2 (
+    id int primary key,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
