759b37f759a849486fdbf0a0bdc2042410b19da6
amitmnayakacc <90895195+amitmnayakacc@users.noreply.github.com>
2021-10-12 16:17:43 +0530

    Update jenkins_changelog_test.sql

--- jenkins_changelog_test.sql
+++ jenkins_changelog_test.sql
@@ -1,5 +1,7 @@
+--liquibase formatted sql
+
 --changeset amit:1
-create table Jenkins_table1 (
+create table person (
     id int primary key,
     name varchar(50) not null,
     address1 varchar(50),
@@ -8,10 +10,13 @@ create table Jenkins_table1 (
 )
 
 --changeset nayak:2
-create table Jenkins_Table2 (
+create table company (
     id int primary key,
     name varchar(50) not null,
     address1 varchar(50),
     address2 varchar(50),
     city varchar(30)
 )
+
+--changeset amit:3
+alter table person add column country varchar(2)
